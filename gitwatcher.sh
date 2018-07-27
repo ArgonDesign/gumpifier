8a526f918cb8ab7867fd2e2d1d21471dfa2152c2
Mohammed Daudali <mohammed.daudali@argondesign.com>
2018-07-27 10:59:32 +0100

    Updated gitwatcher to be more forward

--- gitwatcher.sh
+++ gitwatcher.sh
@@ -1,4 +1,3 @@
 while true; do
-	git fetch -v --dry-run 2>&1 | grep -q "up to date" && (clear; tput setaf 2; echo "GIT not updated") || (tput setaf 1; echo "GIT UPDATED")
-	sleep 60
+	git fetch -v --dry-run 2>&1 | grep -q "up to date" && (clear; tput setaf 2; echo "GIT not updated"; sleep 60) || (clear; tput setaf 1; echo "GIT UPDATED"; echo -e "\a"; sleep 1)
 done
