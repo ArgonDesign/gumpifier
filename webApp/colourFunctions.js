41439b8d7d888c1bfdf28632f4b52485bf02760c
Patrick Taylor <patrick.taylor@argondesign.com>
2018-08-10 15:45:42 +0100

    Uncommented a function which should not have been commented

--- webApp/colourFunctions.js
+++ webApp/colourFunctions.js
@@ -58,20 +58,20 @@ function adjustWhiteBalance(temp) {
 	adjustWhiteBalanceColour(colour);	
 }
 
-// function adjustWhiteBalanceColour(colour) {
-// 	var canvas = document.getElementById('foregroundImage');
-// 	var ctx = canvas.getContext('2d');
-// 	var imageData = ctx.getImageData(0, 0, canvas.width, canvas.height);
-// 	var data = imageData.data;
+function adjustWhiteBalanceColour(colour) {
+	var canvas = document.getElementById('foregroundImage');
+	var ctx = canvas.getContext('2d');
+	var imageData = ctx.getImageData(0, 0, canvas.width, canvas.height);
+	var data = imageData.data;
 
-// 	for (var i = 0; i < data.length; i += 4) {
-// 		data[i] = data[i] * (255/colour.r); // Red
-// 		data[i+1] = data[i+1] * (255/colour.g); // Red
-// 		data[i+2] = data[i+2] * (255/colour.b); // Red
-// 	}
+	for (var i = 0; i < data.length; i += 4) {
+		data[i] = data[i] * (255/colour.r); // Red
+		data[i+1] = data[i+1] * (255/colour.g); // Red
+		data[i+2] = data[i+2] * (255/colour.b); // Red
+	}
 
-// 	ctx.putImageData(imageData, 0, 0);	
-// }
+	ctx.putImageData(imageData, 0, 0);	
+}
 
 function colorTemperatureToRgb(temp) {
 	// Verbatim From CamanJS library
